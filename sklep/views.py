b93441e6853eb71e52cf032b469a3bf73a5f7d03
Łukasz <luken90@wp.pl>
2012-10-22 23:00:55 +0200

    STRONKA STARTOWA
    menu boczne, podstawowy koszyk

---
+++ sklep/views.py
@@ -0,0 +1,76 @@
+# Create your views here.
+# coding: utf-8
+from django.http import Http404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.generic.simple import direct_to_template
+from django.core.mail import send_mail
+from django.template import Context, loader
+from django.conf import settings
+from sklep.models import Towary, Kategorie
+from sklep.forms import ZamowienieForm
+from django.views.generic.list_detail import object_list
+from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
+
+def strona_glowna(request):
+    koszyk = request.session.get('koszyk', [])
+    #if koszyk:
+    #    kontekst = {'koszyk': produkty, 'formularz': formularz}
+    #else:
+    kontekst = {'koszyk': []}
+    
+    return direct_to_template(request, 'sklep/glowna.html', extra_context = kontekst)
+	
+def strona_kontakt(request):
+    koszyk = request.session.get('koszyk', [])
+    #if koszyk:
+    #    kontekst = {'koszyk': produkty, 'formularz': formularz}
+    #else:
+    kontekst = {'koszyk': []}
+    
+    return direct_to_template(request, 'sklep/kontakt.html', extra_context = kontekst)
+def koszyk(request):
+    koszyk = request.session.get('koszyk', [])
+    produkty = list(Towary.objects.filter(pk__in=koszyk))
+    
+    if request.method == 'POST':
+        formularz = ZamowienieForm(request.POST)
+        
+        if formularz.is_valid():
+            dane = formularz.cleaned_data
+            tresc = loader.get_template('sklep/zamowienie.txt').render(Context({'produkty': produkty, 'dane': dane}))
+
+            send_mail('Potwierdzenie zakupu', tresc, settings.EMAIL_SKLEPU, [dane['email']])
+            send_mail(u'Zamowienie', tresc, dane['email'], [settings.EMAIL_SKLEPU])
+            
+            del request.session['koszyk']
+            
+            return HttpResponseRedirect(reverse('sklep_koszyk'))
+    else:
+        formularz = ZamowienieForm()
+    
+    if koszyk:
+        kontekst = {'koszyk': produkty, 'formularz': formularz}
+    else:
+        kontekst = {'koszyk': []}
+    
+    return direct_to_template(request, 'sklep/koszyk.html', extra_context = kontekst)
+
+def koszyk_dodaj(request, id_produktu):
+    koszyk = request.session.get('koszyk', [])
+    if int(id_produktu) not in koszyk:
+        koszyk.append(int(id_produktu))
+    request.session['koszyk'] = koszyk
+    return HttpResponseRedirect(reverse('sklep_koszyk'))
+	
+
+	
+def produkty_z_kategorii(request, id_kategorii):
+    kategoria1 = get_object_or_404(Kategorie, pk=int(id_kategorii))
+    return object_list(
+        request,
+        queryset=Towary.objects.filter(kategoria=kategoria1).select_related('kategoria'),
+        paginate_by=1,
+        extra_context={'kategoria1': kategoria1}
+    )
